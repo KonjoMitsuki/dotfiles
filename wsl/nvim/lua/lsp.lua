f6bfbc067e5b3f7f954facc148a4de1dfa23ac40
satokenji <t2500173@yamanashi.ac.jp>
2026-01-17 23:25:29 +0900

    sshuusei

--- wsl/nvim/lua/lsp.lua
+++ wsl/nvim/lua/lsp.lua
@@ -1,30 +1,93 @@
--- lua/lsp.lua
-local lspconfig = require('lspconfig')
-
--- 例: インストールしたサーバーの設定をここに追加
-local cmp = require('cmp')
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
+-- LSP関連プラグイン
+return {
+  -- LSP設定 (nvim-lspconfig)
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+  },
+  
+  -- Mason (LSPサーバー管理)
+  {
+    "williamboman/mason.nvim",
+    cmd = { "Mason", "MasonInstall", "MasonUpdate" },
+    build = ":MasonUpdate",
+    config = function()
+      require("mason").setup()
     end,
   },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Enterで決定
-    ['<Tab>'] = cmp.mapping.select_next_item(),        -- Tabで次を選択
-    ['<S-Tab>'] = cmp.mapping.select_prev_item(),    -- Shift+Tabで前を選択
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' }, -- LSPからの補完
-    { name = 'luasnip' },  -- スニペット
-  }, {
-    { name = 'buffer' },   -- ファイル内の単語
-  })
-})
--- 必要に応じて、LSP起動時のみ有効にしたいキーマップなどもここに記述できます
+  {
+    "williamboman/mason-lspconfig.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = { "mason.nvim", "nvim-lspconfig" },
+    config = function()
+      require("mason-lspconfig").setup()
+    end,
+  },
 
--- Treesitter に関連する require や setup をコメントアウト
--- require("nvim-treesitter.configs").setup({...})
+  -- 補完 (nvim-cmp)
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "saadparwaiz1/cmp_luasnip",
+      "L3MON4D3/LuaSnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end,
+  },
+
+  -- スニペット (LuaSnip)
+  {
+    "L3MON4D3/LuaSnip",
+    event = "InsertEnter",
+    config = function()
+      require("luasnip").setup()
+    end,
+  },
+
+  -- シンタックスハイライト (Treesitter)
+  -- 以前のファイルでここが壊れていました
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "c", "cpp", "python", "lua", "vim", "vimdoc", "query" }, -- 必要な言語を追加
+        highlight = {
+          enable = true,
+          disable = function(lang, buf)
+            -- 大きなファイルでは無効にするなどの設定が可能
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
+        },
+      })
+    end,
+  },
+}
--- wsl/nvim/lua/plugins/lsp.lua
+++ wsl/nvim/lua/plugins/lsp.lua
@@ -1,9 +1,12 @@
 -- LSP関連プラグイン
 return {
+  -- 1. LSP設定 (nvim-lspconfig)
   {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
   },
+
+  -- 2. Mason (LSPサーバー管理)
   {
     "williamboman/mason.nvim",
     cmd = { "Mason", "MasonInstall", "MasonUpdate" },
@@ -20,6 +23,8 @@ return {
       require("mason-lspconfig").setup()
     end,
   },
+
+  -- 3. 補完 (nvim-cmp)
   {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
@@ -52,6 +57,8 @@ return {
       })
     end,
   },
+
+  -- 4. スニペット (LuaSnip)
   {
     "L3MON4D3/LuaSnip",
     event = "InsertEnter",
@@ -59,19 +66,33 @@ return {
       require("luasnip").setup()
     end,
   },
+
+  -- 5. シンタックスハイライト (Treesitter)
   {
-"neovim/nvim-lspconfig",
-  dependencies = {
-    {
-      "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate",
-      config = function()
-        require("nvim-treesitter.configs").setup({
-          highlight = { enable = true },
-        })
-            disable = { "vim" },
-          },
-        })
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      -- 【修正点】ここで安全確認を行います
+      -- プラグインがまだインストールされていない場合は、エラーを出さずに終了します
+      local status, configs = pcall(require, "nvim-treesitter.configs")
+      if not status then
+        return
+      end
+
+      configs.setup({
+        ensure_installed = { "c", "cpp", "python", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+        highlight = {
+          enable = true,
+          disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
+        },
+      })
     end,
   },
 }
