f56a53c21232dc9a264c8bc493693b884f47721e
Siiiron laptop <satomataru@gmail.com>
2026-06-01 14:16:54 +0900

    テーマをellisonleao/gruvbox.nvimに変更

--- wsl/nvim/lua/plugins/theme.lua
+++ wsl/nvim/lua/plugins/theme.lua
@@ -1,33 +1,13 @@
--- テーマ・カラースキーム
 return {
   {
-    "rebelot/kanagawa.nvim",
-    priority = 1000,
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000, 
     config = function()
-      require("kanagawa").setup({
-        -- Neovim の背景を透過にする
-        transparent = true,
-        
-        -- テーマのバリエーション（"wave", "dragon", "lotus" から選択可）
-        theme = "wave",
-
-        -- フローティングウィンドウの見た目を少し整える
-        overrides = function(colors)
-          return {
-            Cursor = { fg = "black", bg = "white" },
-            TermCursor = { fg = "black", bg = "white" },
-            NormalFloat = { bg = "none" },
-            FloatBorder = { bg = "none" },
-            Floating = { bg = "none" },
-
-            -- 必要なら行番号の背景も消せる
-            -- LineNr = { bg = "none" },
-          }
-        end,
+      require("gruvbox").setup({
+        contrast = "hard", 
+        transparent_mode = false,
       })
-
-      -- テーマを有効化する
-      vim.cmd("colorscheme kanagawa")
+      vim.cmd("colorscheme gruvbox")
     end,
-  },
+  }
 }
